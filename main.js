a8aa7782cbab70d6b7831bd71feda406e63640ae
daria-chr <cherkinadar@gmail.com>
2026-03-27 12:58:27 +0300

    Step 6: input and button interaction

--- main.js
+++ main.js
@@ -25,3 +25,17 @@ btn.addEventListener("click",()=>{
     btn.style.color="white";
 });
 
+const input=document.getElementById("nameInput");
+const button=document.getElementById("showName");
+const output=document.getElementById("output");
+
+button.addEventListener("click", () =>{
+    if (input.value.trim()!==""){
+        output.textContent=`Привет ${input.value}`;
+        output.style.color="green";
+    }else{
+        output.textContent="пожалуйста введите имя";
+        output.style.color="red";
+    }
+});
+
